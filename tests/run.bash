b7c94158711289959b1adeaf120c6805635031c1
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-03-31 09:48:34 +0100

    (feat): Added new functionality tests

--- tests/run.bash
+++ tests/run.bash
@@ -15,6 +15,7 @@ fi
 testone() {
 	local module="$1" # Professor/Client/Admin
 	local test_name="$2"
+	local arguments="$3"
 	
 	local input_file="./input/${test_name}.in"
 	local expected_output_file="./expected/${test_name}.out"
@@ -22,41 +23,11 @@ testone() {
 	
 	local expected_line_count="$(( $(wc -l < "${expected_output_file}") + 1))"
 
-	local student_id=""
-	local student_name=""
-
-  if [ "$test_name" = "02-student" ]; then
-    # Cristina Ferreira
-    student_id="aluno1000"
-    student_name="'Cristina Ferreira'"
-  elif [ "$test_name" = "03-student" ]; then
-    # Goucha
-    student_id="aluno1001"
-    student_name="'Manuel Goucha'"
-  elif [ "$test_name" = "05-student" ]; then
-    student_id="aluno1001"
-    student_name="'Manuel Goucha'"
-  elif [ "$test_name" = "07-student" ]; then
-    # 3a pessoa
-    student_id="aluno1002"
-    student_name="'Castelo Branco'"
-  fi
-
-  if [ "$student_id" != "" ]; then
-    local args="-Dexec.args=localhost 2001 ${student_id} ${student_name}"
-    echo "Using ARGS: $args"
-    (
-    		set -e # exit (this subshell) on first error
-    		(cd ..; mvn -q -pl "${module}" exec:java "${args}") < "${input_file}" > "${actual_output_file}" 2>/dev/null
-    		"${DIFF}" -w "${expected_output_file}" <(tail -n "${expected_line_count}" "${actual_output_file}")
-    	)
-  else
-    (
-        set -e # exit (this subshell) on first error
-        (cd ..; mvn -q -pl "${module}" exec:java) < "${input_file}" > "${actual_output_file}" 2>/dev/null
-        "${DIFF}" -w "${expected_output_file}" <(tail -n "${expected_line_count}" "${actual_output_file}")
-      )
-  fi
+	(
+		set -e # exit (this subshell) on first error
+		(cd ..; mvn -q -pl "${module}" -Dexec.args="$arguments" exec:java) < "${input_file}" > "${actual_output_file}" 2>/dev/null
+		"${DIFF}" -w "${expected_output_file}" <(tail -n "${expected_line_count}" "${actual_output_file}")
+	)
 }
 
 passed_count=0
@@ -66,12 +37,14 @@ for test_input_file in ./input/*.in; do
 	test_name="$(basename "$test_input_file")" # extract filename from path
 	test_name="${test_name%.in}" # remove extension
 	module="$(echo "$test_name" | cut -d- -f2)" # extract module
-	# module="${module^}" # make first letter of module uppercase
-	module="$(tr '[:lower:]' '[:upper:]' <<< "${module:0:1}")${module:1}"
+	arguments="$(echo "$test_name" | cut -d- -f3- | sed "s/-/' '/" | sed "s/.*/'&'/")" # do some basic argument pre-processing: wrap each argument in quotes to deal with spaces properly
 
 	test_count="$(( test_count + 1 ))"
 
-	if testone "$module" "$test_name"; then
+	echo
+	echo
+	echo "Testing $test_name"
+	if testone "$module" "$test_name" "$arguments"; then
 		passed_count="$(( passed_count + 1 ))"
 		echo -e "${GREEN}${test_name}: Success!${RESET}"
 	else
