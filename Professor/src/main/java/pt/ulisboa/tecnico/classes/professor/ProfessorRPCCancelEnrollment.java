516f8fe32da441fe69e829f2145ba6dcc871bf47
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-04-25 22:23:03 +0100

    (feat): Created concrete class for Professor CancelEnrollment rpc call

---
+++ Professor/src/main/java/pt/ulisboa/tecnico/classes/professor/ProfessorRPCCancelEnrollment.java
@@ -0,0 +1,42 @@
+package pt.ulisboa.tecnico.classes.professor;
+
+import pt.ulisboa.tecnico.classes.NameServerFrontend;
+import pt.ulisboa.tecnico.classes.RPCFrontendCall;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
+import pt.ulisboa.tecnico.classes.contract.professor.ProfessorClassServer;
+import pt.ulisboa.tecnico.classes.contract.professor.ProfessorServiceGrpc;
+
+import java.util.List;
+
+public class ProfessorRPCCancelEnrollment extends RPCFrontendCall {
+
+    private ProfessorServiceGrpc.ProfessorServiceBlockingStub _stub;
+    private final NameServerFrontend _nameServer;
+    private ProfessorClassServer.CancelEnrollmentResponse _response;
+    private String _studentId;
+
+
+    public ProfessorRPCCancelEnrollment(List<String> qualifiers, NameServerFrontend nameServer) {
+        super(qualifiers);
+        _nameServer = nameServer;
+    }
+
+    public void setStudentId(String studentId) {
+        this._studentId = studentId;
+    }
+
+    public void createStubForRequest(List<String> qualifiers, boolean previousIsInactive) {
+        this._stub = ProfessorServiceGrpc.newBlockingStub(_nameServer.getChannel(ProfessorServiceGrpc.SERVICE_NAME, qualifiers, previousIsInactive));
+    }
+
+    public ClassesDefinitions.ResponseCode requestCall() {
+        ProfessorClassServer.CancelEnrollmentRequest request = ProfessorClassServer.CancelEnrollmentRequest.newBuilder().setStudentId(this._studentId).build();
+        this._response = _stub.cancelEnrollment(request);
+
+        return this._response.getCode();
+    }
+
+    public ProfessorClassServer.CancelEnrollmentResponse getResponse() {
+        return _response;
+    }
+}
