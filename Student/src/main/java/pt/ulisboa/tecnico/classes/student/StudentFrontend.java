271f35b3e2b0e21f5fd040f1335c529365449e4e
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-04-25 22:25:23 +0100

    (feat): Added concrete classes: StudentRPCEnroll, StudentRPCList to StudentFrontend

--- Student/src/main/java/pt/ulisboa/tecnico/classes/student/StudentFrontend.java
+++ Student/src/main/java/pt/ulisboa/tecnico/classes/student/StudentFrontend.java
@@ -1,22 +1,17 @@
 package pt.ulisboa.tecnico.classes.student;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 import pt.ulisboa.tecnico.classes.NameServerFrontend;
 import pt.ulisboa.tecnico.classes.ResponseException;
-import pt.ulisboa.tecnico.classes.Stringify;
 import pt.ulisboa.tecnico.classes.TimestampsManager;
 import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.Student;
 import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.ResponseCode;
 import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.ClassState;
-import pt.ulisboa.tecnico.classes.contract.professor.ProfessorServiceGrpc;
 import pt.ulisboa.tecnico.classes.contract.student.StudentClassServer;
 import pt.ulisboa.tecnico.classes.contract.student.StudentServiceGrpc;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /** This class abstracts all the stub calls executed by the Student client */
 public class StudentFrontend extends TimestampsManager {
@@ -37,26 +32,17 @@ public class StudentFrontend extends TimestampsManager {
     this._student = Student.newBuilder().setStudentId(studentID).setStudentName(studentName).build();
   }
 
-  private void getNewStubWithQualifiers(List<String> qualifiers, boolean previousIsInactive) {
-    this._stub = StudentServiceGrpc.newBlockingStub(_nameServer.getChannel(StudentServiceGrpc.SERVICE_NAME, qualifiers, previousIsInactive));
-  }
-
   /**
    * Sends an enroll request to the server and enrolls a student if is possible * prints the
    * response code
    */
   public ResponseCode enrollStudent() throws StatusRuntimeException, ResponseException {
-    getNewStubWithQualifiers(new ArrayList<>(), false);
+    StudentRPCEnroll rpcCall = new StudentRPCEnroll(new ArrayList<>(), this._nameServer);
 
-    StudentClassServer.EnrollRequest request = StudentClassServer.EnrollRequest.newBuilder().setStudent(this._student).build();
-    StudentClassServer.EnrollResponse response = _stub.enroll(request);
+    rpcCall.setStudent(this._student);
+    rpcCall.exec();
 
-    if (response.getCode() == ResponseCode.INACTIVE_SERVER) {
-      getNewStubWithQualifiers(new ArrayList<>(), true);
-      return this.enrollStudent();
-    }
-
-    return response.getCode();
+    return rpcCall.getResponse().getCode();
   }
 
   /**
@@ -67,24 +53,11 @@ public class StudentFrontend extends TimestampsManager {
    * @throws ResponseException
    */
   public ClassState list() throws StatusRuntimeException, ResponseException {
-    getNewStubWithQualifiers(new ArrayList<>(), false);
+    StudentRPCList rpcCall = new StudentRPCList(new ArrayList<>(), this._nameServer);
 
-    StudentClassServer.ListClassRequest.Builder request = StudentClassServer.ListClassRequest.newBuilder();
+    rpcCall.setTimestamps(this.getTimestamps());
+    rpcCall.exec();
 
-    request.putAllTimestamps(this.getTimestamps());
-
-    StudentClassServer.ListClassResponse response = _stub.listClass(request.build());
-
-    if (response.getCode() == ResponseCode.INACTIVE_SERVER) {
-      getNewStubWithQualifiers(new ArrayList<>(), true);
-      return this.list();
-    }
-    else if (response.getCode() == ResponseCode.OK) {
-      this.updateTimestamps(response.getTimestampsMap());
-      return response.getClassState();
-    }
-    else {
-      throw new ResponseException(response.getCode());
-    }
+    return rpcCall.getResponse().getClassState();
   }
 }
