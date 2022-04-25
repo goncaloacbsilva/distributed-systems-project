238827c70aa4bbbf6713e281b54f0dbe365c779a
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2022-04-25 22:20:52 +0100

    (feat): Created concrete class for Student Enroll rpc call

---
+++ Student/src/main/java/pt/ulisboa/tecnico/classes/student/StudentRPCEnroll.java
@@ -0,0 +1,45 @@
+package pt.ulisboa.tecnico.classes.student;
+
+import pt.ulisboa.tecnico.classes.NameServerFrontend;
+import pt.ulisboa.tecnico.classes.RPCFrontendCall;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
+import pt.ulisboa.tecnico.classes.contract.student.StudentClassServer;
+import pt.ulisboa.tecnico.classes.contract.student.StudentServiceGrpc;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class StudentRPCEnroll extends RPCFrontendCall {
+
+    private StudentServiceGrpc.StudentServiceBlockingStub _stub;
+    private final NameServerFrontend _nameServer;
+    private StudentClassServer.EnrollResponse _response;
+
+    private ClassesDefinitions.Student _student;
+
+
+    public StudentRPCEnroll(List<String> qualifiers, NameServerFrontend nameServer) {
+        super(qualifiers);
+        _nameServer = nameServer;
+    }
+
+    public void setStudent(ClassesDefinitions.Student student) {
+        this._student = student;
+    }
+
+    public void createStubForRequest(List<String> qualifiers, boolean previousIsInactive) {
+        this._stub = StudentServiceGrpc.newBlockingStub(_nameServer.getChannel(StudentServiceGrpc.SERVICE_NAME, qualifiers, previousIsInactive));
+    }
+
+    public ClassesDefinitions.ResponseCode requestCall() {
+        StudentClassServer.EnrollRequest request = StudentClassServer.EnrollRequest.newBuilder().setStudent(this._student).build();
+
+        this._response = _stub.enroll(request);
+
+        return this._response.getCode();
+    }
+
+    public StudentClassServer.EnrollResponse getResponse() {
+        return _response;
+    }
+}
