3b6544d0c8798ab5ae6856f3ed9464ea158b66a9
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-04-26 22:24:43 +0100

    (chore): Added JavaDoc for AdminRPCDump

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCDump.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCDump.java
@@ -11,24 +11,28 @@ import pt.ulisboa.tecnico.classes.contract.professor.ProfessorServiceGrpc;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Dump command RPC Class
+ */
 public class AdminRPCDump extends RPCFrontendCall {
 
     private AdminServiceGrpc.AdminServiceBlockingStub _stub;
-    private final NameServerFrontend _nameServer;
     private AdminClassServer.DumpResponse _response;
 
-
-    public AdminRPCDump(List<String> qualifiers, NameServerFrontend nameServer) {
+    /**
+     * Creates an instance of AdminRPCDump
+     *
+     * @see AdminRPCDump
+     *
+     * @param qualifiers server qualifiers list
+     */
+    public AdminRPCDump(List<String> qualifiers) {
         super(qualifiers);
-        _nameServer = nameServer;
     }
 
     public void createStubForRequest(List<String> qualifiers, boolean previousIsInactive) {
-        // Admin has a different behavior
-    }
-
-    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub _stub) {
-        this._stub = _stub;
+        // Admin has a different behavior than the 'default' clients so this method is not used
+        // The service stub is set using setStub method
     }
 
     public ClassesDefinitions.ResponseCode requestCall() {
@@ -37,6 +41,20 @@ public class AdminRPCDump extends RPCFrontendCall {
         return this._response.getCode();
     }
 
+    /**
+     * Sets the stub for the RPC call
+     * @param stub AdminServiceBlockingStub
+     */
+    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub _stub) {
+        this._stub = _stub;
+    }
+
+    /**
+     * Returns the response of the RPC call.
+     * It should only be called after the exec() method otherwise its value is null.
+     *
+     * @return AdminClassServer.DumpResponse
+     */
     public AdminClassServer.DumpResponse getResponse() {
         return _response;
     }
