2b763ba28114e0e764f6bc049c00078552cf0dc1
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-04-26 22:23:06 +0100

    (chore): Added JavaDoc for AdminRPCActivate

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCActivate.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCActivate.java
@@ -9,26 +9,27 @@ import pt.ulisboa.tecnico.classes.contract.admin.AdminServiceGrpc;
 
 import java.util.List;
 
+/**
+ * Activate command RPC Class
+ */
 public class AdminRPCActivate extends RPCFrontendCall {
-
     private AdminServiceGrpc.AdminServiceBlockingStub _stub;
-
-    private final NameServerFrontend _nameServer;
-
     private AdminClassServer.ActivateResponse _response;
 
-    public AdminRPCActivate(List<String> qualifiers, NameServerFrontend nameServer) {
+    /**
+     * Creates an instance of AdminRPCActivate
+     *
+     * @see AdminRPCActivate
+     *
+     * @param qualifiers server qualifiers list
+     */
+    public AdminRPCActivate(List<String> qualifiers) {
         super(qualifiers);
-        _nameServer = nameServer;
     }
-
 
     public void createStubForRequest(List<String> qualifiers, boolean previousIsInactive) {
-        // Admin has a different behavior
-    }
-
-    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub stub) {
-        this._stub = stub;
+        // Admin has a different behavior than the 'default' clients so this method is not used
+        // The service stub is set using setStub method
     }
 
     public ClassesDefinitions.ResponseCode requestCall() throws StatusRuntimeException {
@@ -37,6 +38,20 @@ public class AdminRPCActivate extends RPCFrontendCall {
         return this._response.getCode();
     }
 
+    /**
+     * Sets the stub for the RPC call
+     * @param stub AdminServiceBlockingStub
+     */
+    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub stub) {
+        this._stub = stub;
+    }
+
+    /**
+     * Returns the response of the RPC call.
+     * It should only be called after the exec() method otherwise its value is null.
+     *
+     * @return AdminClassServer.ActivateResponse
+     */
     public AdminClassServer.ActivateResponse getResponse() {
         return _response;
     }
