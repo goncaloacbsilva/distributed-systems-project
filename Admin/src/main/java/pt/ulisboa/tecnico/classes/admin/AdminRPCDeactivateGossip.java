36219b1dda8fb792b7b7e36c960ffa0871bf7e20
Gonçalo Abreu Corrêa Brito Da Silva <goncaloasilva@edu.ulisboa.pt>
2022-04-26 22:24:22 +0100

    (chore): Added JavaDoc for AdminRPCDeactivateGossip

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCDeactivateGossip.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminRPCDeactivateGossip.java
@@ -11,24 +11,28 @@ import pt.ulisboa.tecnico.classes.contract.professor.ProfessorServiceGrpc;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Deactivate Gossip command RPC Class
+ */
 public class AdminRPCDeactivateGossip extends RPCFrontendCall {
 
     private AdminServiceGrpc.AdminServiceBlockingStub _stub;
-    private final NameServerFrontend _nameServer;
     private AdminClassServer.DeactivateGossipResponse _response;
 
-
-    public AdminRPCDeactivateGossip(List<String> qualifiers, NameServerFrontend nameServer) {
+    /**
+     * Creates an instance of AdminRPCDeactivateGossip
+     *
+     * @see AdminRPCDeactivateGossip
+     *
+     * @param qualifiers server qualifiers list
+     */
+    public AdminRPCDeactivateGossip(List<String> qualifiers) {
         super(qualifiers);
-        _nameServer = nameServer;
     }
 
     public void createStubForRequest(List<String> qualifiers, boolean previousIsInactive) {
-        // Admin has a different behavior
-    }
-
-    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub _stub) {
-        this._stub = _stub;
+        // Admin has a different behavior than the 'default' clients so this method is not used
+        // The service stub is set using setStub method
     }
 
     public ClassesDefinitions.ResponseCode requestCall() {
@@ -37,6 +41,20 @@ public class AdminRPCDeactivateGossip extends RPCFrontendCall {
         return this._response.getCode();
     }
 
+    /**
+     * Sets the stub for the RPC call
+     * @param stub AdminServiceBlockingStub
+     */
+    public void setStub(AdminServiceGrpc.AdminServiceBlockingStub _stub) {
+        this._stub = _stub;
+    }
+
+    /**
+     * Returns the response of the RPC call.
+     * It should only be called after the exec() method otherwise its value is null.
+     *
+     * @return AdminClassServer.DeactivateGossipResponse
+     */
     public AdminClassServer.DeactivateGossipResponse getResponse() {
         return _response;
     }
